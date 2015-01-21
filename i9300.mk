c970fd86fcec7e9ab0499c3620d695d8c69818be
MarcKe <herderkewitz@googlemail.com>
2015-01-21 21:26:01 +0100

    i9300: init.target.rc: add vnswap configuration

--- i9300.mk
+++ i9300.mk
@@ -97,4 +97,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+# vnswap
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/configure_vnswap:system/bin/configure_vnswap
+
 $(call inherit-product-if-exists, vendor/samsung/i9300/i9300-vendor.mk)
