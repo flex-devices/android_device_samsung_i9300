fe49deeda822ea91507c079b4289d5d3b134ff0f
MarcKe <herderkewitz@googlemail.com>
2015-02-13 08:20:19 +0100

    i9300: add zram configuration file
    handling more than 1 zram device does not seem to be possible via fstab
    so this is needed

--- i9300.mk
+++ i9300.mk
@@ -97,9 +97,12 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-$(call inherit-product-if-exists, vendor/samsung/i9300/i9300-vendor.mk)
-
+# ZRAM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/configure_zram:system/bin/configure_zram
 
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
+
+$(call inherit-product-if-exists, vendor/samsung/i9300/i9300-vendor.mk)
